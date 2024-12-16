c03913f09832545cc5a4bbd8e6da596faa89d654
zoé <fkfhjti@gmail.com>
2024-12-16 21:38:58 +0900

    241216 코딩테스트 연습 01.문자열 출력하기

---
+++ ct_01/solution/sol_02.js
@@ -0,0 +1,7 @@
+const readline = require("readline");
+const rl = readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", console.log);
---
+++ ct_01/solution/sol_03.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const str = input.join(" ");
+  console.log(str);
+});
