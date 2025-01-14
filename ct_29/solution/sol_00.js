2ff62685afb47f0a3944b1682d826e0f5ada5d99
zoé <fkfhjti@gmail.com>
2025-01-14 14:15:39 +0900

    250114 코딩테스트 연습 29.n의 배수 고르기

---
+++ ct_29/solution/sol_00.js
@@ -0,0 +1,10 @@
+function solution(n, numlist) {
+  var answer = [];
+  for (num of numlist) {
+    if (num % n === 0) answer.push(num);
+  }
+  console.log(answer);
+  return answer;
+}
+
+solution(3, [4, 5, 6, 7, 8, 9, 10, 11, 12]);
