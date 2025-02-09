035e45e5aa25845b0f60c5b97c18c1761a616bc2
zoé <fkfhjti@gmail.com>
2025-02-09 19:25:11 +0900

    250209 코딩테스트 리뷰 11-12

--- reviewed/ct_11/solution/sol_00.js
+++ reviewed/ct_11/solution/sol_00.js
@@ -1,11 +1,9 @@
 function solution(s) {
   const _s = [...s];
-  console.log("_s = ", _s);
+
   const sArray = _s.filter(function (item) {
     return item !== " ";
   });
-  console.log("_s = ", _s);
-  console.log("sArray = ", sArray);
 
   let sum = 0;
 
@@ -14,16 +12,9 @@ function solution(s) {
     else sArray[i] = parseInt(sArray[i], 10);
   }
 
-  console.log("sArray = ", sArray);
-
   sArray.forEach(function (element) {
     sum += element;
   });
 
-  console.log(sum);
-
   return sum;
 }
-
-solution("1 2 Z 3");
-solution("10 Z 20 Z");
