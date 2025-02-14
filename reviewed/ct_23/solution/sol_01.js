eb50aac3812c592e6a58ebffccb89b9d9fc523de
zoé <fkfhjti@gmail.com>
2025-02-14 21:25:45 +0900

    250214 코딩테스트 리뷰 23-25

--- reviewed/ct_23/solution/sol_01.js
+++ reviewed/ct_23/solution/sol_01.js
@@ -6,10 +6,9 @@ function solution(quiz) {
     const x = parseInt(parts[0]);
     const op = parts[1]; // "-"
     const y = parseInt(parts[2]); // "4"
-    // parts[3]는 "="
+
     const z = parseInt(parts[4]); // "-3"
 
-    // 실제 계산
     let isCorrect = false;
     if (op === "+") {
       isCorrect = x + y === z;
@@ -17,7 +16,6 @@ function solution(quiz) {
       isCorrect = x - y === z;
     }
 
-    // 결과 배열에 추가
     result.push(isCorrect ? "O" : "X");
   }
   return result;
