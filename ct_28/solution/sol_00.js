7fcf7fbbddc1c01153a60abf525825994fab6840
zoé <fkfhjti@gmail.com>
2025-02-16 20:00:50 +0900

    250216 코딩테스트 리뷰 28-30

--- ct_28/solution/sol_00.js
+++
@@ -1,8 +0,0 @@
-function solution(box, n) {
-  var answer = 0;
-  answer =
-    Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
-  return answer;
-}
-
-solution([10, 8, 6], 3);
